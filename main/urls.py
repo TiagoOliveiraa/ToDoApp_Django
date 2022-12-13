6d240702c299ed20d13a495bd00e89ac9514eb44
Mythicale <106434966+Mythicale@users.noreply.github.com>
2022-12-13 22:56:56 +0000

    All CRUD operations

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,9 @@ from .views import taskList, taskDetail, taskCreate, taskDelete, taskUpdate
 
 urlpatterns = [
     path('', taskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', taskDetail.as_view(), name='tasks'),
+    path('task-create/',taskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', taskUpdate.as_view(), name="task-update"),
+    path('task-delete/<int:pk>/', taskDelete.as_view(), name='task-delete'),
     
 ]
--- main/views.py
+++ main/views.py
@@ -11,25 +11,29 @@ def test(request):
 class taskList(ListView):
     
     model = Task
+    context_object_name = 'tasks'
     
 class taskDetail(DetailView):
     
     model = Task
+    context_object_name = 'task'
     
 class taskCreate(CreateView):
     
     model = Task
-    fields = ['title', 'decription', 'complete']
-    success_url = reverse_lazy('tasks/')
+    context_object_name = 'task'
+    #fields = ['title', 'description', 'complete']
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
     
 class taskUpdate(UpdateView):
     
     model = Task
-    fields = ['title', 'description', 'complete']
-    success_url = reverse_lazy('tasks/')
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
     
 class taskDelete(DeleteView):
     
     model = Task
     context_object_name = 'task'
-    success_url = reverse_lazy('tasks/')
+    success_url = reverse_lazy('tasks')
